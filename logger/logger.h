2a85266c7918e9cac778b8fd3702c8bd12db11a5
Florian Lardeux <magnaflo35@gmail.com>
2022-10-16 17:58:41 +0200

    commented logger

--- logger/logger.h
+++ logger/logger.h
@@ -13,6 +13,13 @@ void clear_log(std::string file_name)
     ofs.close();
 }
 
+// Time Logger function
+// Empty call returns a time_point
+// Full writes in log 
+// args: > file: file stream to write in 
+//		 > t: time_point to mark as the end time for the log 
+//		 > func: name of the function / part of the code (include 'total' 
+//				 to make it a total function
 auto TL(std::ofstream* file=nullptr, std::chrono::steady_clock::time_point t = std::chrono::steady_clock::now(), std::string func = "")
 {
     std::chrono::steady_clock::time_point current = std::chrono::steady_clock::now();
